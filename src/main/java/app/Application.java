2a0c03156ada3210ecf36ce3a72c52cae923827d
Basov Alexandr <bainst4@gmail.com>
2016-11-11 15:53:43 +0300

    renamed hotel -> app

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,4 @@
-package hotel;
+package app;
 
 import spark.route.RouteOverview;
 
--- src/main/java/app/db/DatabaseConnectionTest.java
+++ src/main/java/app/db/DatabaseConnectionTest.java
@@ -1,4 +1,4 @@
-package hotel.db;
+package app.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
