dff4656346b23ea3b5c37e04dca85f20afd1723c
Basov Alexandr <bainst4@gmail.com>
2016-11-15 17:52:42 +0300

    + all hotels method

--- src/main/java/app/hotels/Hotel.java
+++ src/main/java/app/hotels/Hotel.java
@@ -3,11 +3,22 @@ package app.hotels;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 @Data @AllArgsConstructor
 public class Hotel {
 
     int id;
     String city;
     String address;
+
+    public static List<Hotel> all() {
+        return HotelsDao.selectAll().stream()
+                .sorted(comparing(Hotel::getCity))
+                .collect(toList());
+    }
 
 }
