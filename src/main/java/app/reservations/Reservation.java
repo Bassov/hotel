8a5aaf07f84fa9e0d35a78d41e00908df4bcf592
VecnaSecrets <teslartifex@gmail.com>
2016-11-15 13:45:52 +0300

    added Reservation

---
+++ src/main/java/app/reservations/Reservation.java
@@ -0,0 +1,19 @@
+package app.reservations;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data @AllArgsConstructor
+public class Reservation {
+
+    int id;
+    String guest_mail;
+    int room_number;
+    int hotel_id;
+    Timestamp st_date;
+    Timestamp end_date;
+    int key_id;
+
+}
